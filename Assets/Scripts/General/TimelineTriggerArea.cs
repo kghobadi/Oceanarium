795db52d25299953a3edf61516125f7b042bede4
Unknown <ksg304@nyu.edu>
2019-12-16 16:18:48 -0500

    cinematics in place for the Current activations in planet test

---
+++ Assets/Scripts/General/TimelineTriggerArea.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimelineTriggerArea : MonoBehaviour {
+
+	[Header("Component References")]
+	public TimelinePlaybackManager timelinePlaybackManager;
+
+	[Header("Settings")]
+	public string playerString = "Player";
+
+    public bool hasActivated;
+
+
+	void OnTriggerEnter(Collider theCollision){
+        if(theCollision.gameObject.tag == playerString && !hasActivated)
+        {
+            timelinePlaybackManager.PlayerEnteredZone();
+            hasActivated = true;
+        }
+		
+	}
+
+	void OnTriggerExit(Collider theCollision){
+        if (theCollision.gameObject.tag == playerString)
+        {
+            timelinePlaybackManager.PlayerExitedZone();
+        }
+       
+	}
+}
