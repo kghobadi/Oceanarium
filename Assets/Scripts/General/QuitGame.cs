384510f6337a52718ec652a7187c113806e1ab9d
Kas <ksg304@nyu.edu>
2020-08-27 16:06:22 -0400

    added modular menu Selections script for navigating any menu using the star selectors. updated the prefab accordingly, should work with the controller fully now.

--- Assets/Scripts/General/QuitGame.cs
+++ Assets/Scripts/General/QuitGame.cs
@@ -5,22 +5,20 @@ using UnityEngine.SceneManagement;
 using InControl;
 
 public class QuitGame : MonoBehaviour {
+    
+    MenuSelections mainMenuSelections;
+
     public GameObject escMenu;
-
-    [Header("Esc Menu Selections & Sub Menus")]
-    public StarSelectors[] menuSelections;
-    public GameObject[] subMenus;
-    public int currentSelector = 0;
-
     [Header("Controls UI")]
     public GameObject controlsUI;
     public GameObject controlsKeyboard;
     public GameObject controllerInputs;
 
-    //for controller selections 
-    bool canChange;
-    float changeTimer, changeReset = 0.25f;
-    
+    void Awake()
+    {
+        mainMenuSelections = GetComponent<MenuSelections>();
+    }
+
     void Start()
     {
         escMenu.SetActive(false);
@@ -32,21 +30,7 @@ public class QuitGame : MonoBehaviour {
 
         //get input device 
         var inputDevice = InputManager.ActiveDevice;
-
-        //controller 
-        if (inputDevice.DeviceClass == InputDeviceClass.Controller)
-        {
-            //handles controller inputs for the menu
-            ControllerSelection();
-            //resets when you change selectors
-            ChangeReset();
-        }
-        //mouse & keyboard
-        else
-        {
-
-        }
-
+        
         //activate quit group   
         if ((Input.GetKeyDown(KeyCode.Escape) ||  inputDevice.Command.WasPressed) && escMenu.activeSelf == false && !pressed)
         {
@@ -58,112 +42,21 @@ public class QuitGame : MonoBehaviour {
         //either turns off all sub menus, or leaves esc menu 
         if ((Input.GetKeyDown(KeyCode.Escape) || inputDevice.Command.WasPressed) && escMenu.activeSelf == true && !pressed)
         {
-            bool subMenus = CheckSubMenusActive();
+            bool subMenus = mainMenuSelections.CheckSubMenusActive();
 
             if (subMenus)
-                DeactivateAllSubMenus();
+                mainMenuSelections.DeactivateAllSubMenus();
             else
+            {
+                mainMenuSelections.DeactivateMenu();
                 DeactivateObj(escMenu);
+            }
+                
 
             pressed = true;
         }
     }
-
-    //function controls selection with controller
-    void ControllerSelection()
-    {
-        //get input device 
-        var inputDevice = InputManager.ActiveDevice;
-
-        float inputValY = inputDevice.LeftStickY;
-
-        //detection of changing 
-        if (canChange)
-        {
-            //pos val, selection moves up
-            if (inputValY > 0)
-            {
-                ChangeMenuSelector(true);
-            }
-            //neg val, selection moves down
-            else if (inputValY < 0)
-            {
-                ChangeMenuSelector(false);
-            }
-        }
-
-        //detection of selection
-        if (inputDevice.Action1.WasPressed)
-        {
-            if (menuSelections[currentSelector])
-                menuSelections[currentSelector].SelectMe();
-        }
-
-        //detection of closing submenus
-        if (inputDevice.Action2.WasPressed)
-        {
-            DeactivateAllSubMenus();
-        }
-    }
-
-    //allows you to increment menu selector & change stars
-    public void ChangeMenuSelector(bool upOrDown)
-    {
-        if (menuSelections.Length > 1)
-        {
-            //deactivate current stars
-            menuSelections[currentSelector].DeactivateStars();
-
-            //up
-            if (upOrDown)
-            {
-                //increment up
-                if (currentSelector < menuSelections.Length - 1)
-                {
-                    currentSelector++;
-                }
-                else
-                {
-                    currentSelector = 0;
-                }
-            }
-            //down
-            else
-            {
-                //increment down
-                if (currentSelector > 0)
-                {
-                    currentSelector--;
-                }
-                else
-                {
-                    currentSelector = menuSelections.Length - 1;
-                }
-            }
-
-            //activate next stars
-            menuSelections[currentSelector].ActivateStars();
-
-            //change reset called 
-            canChange = false;
-            changeTimer = 0;
-        }
-    }
-
-    //handles reset timer for controller selection
-    void ChangeReset()
-    {
-        if(canChange == false)
-        {
-            changeTimer += Time.deltaTime;
-
-            if(changeTimer > changeReset)
-            {
-                canChange = true;
-            }
-        }
-    }
-
+    
     //called to open esc menu 
     public void ActivateQuitMenu()
     {
@@ -171,32 +64,10 @@ public class QuitGame : MonoBehaviour {
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.None;
 
-        //set Selector
-        currentSelector = 0;
-        if(menuSelections.Length > 0)
-            menuSelections[currentSelector].ActivateStars();
+        //activate menu & set Selectors
+        mainMenuSelections.ActivateMenu();
     }
-
-    //checks if there is a sub menu open
-    bool CheckSubMenusActive()
-    {
-        for(int i = 0; i < subMenus.Length; i++)
-        {
-            if (subMenus[i].activeSelf)
-                return true;
-        }
-
-        return false;
-    }
-
-    //turns off all subMenus
-    public void DeactivateAllSubMenus()
-    {
-        for (int i = 0; i < subMenus.Length; i++)
-        {
-            subMenus[i].SetActive(false);
-        }
-    }
+    
 
     public void ShowControls()
     {
