1d7ac119955d70beb2b8a3520f4289c69907d516
kghobadi <kasghobadi@gmail.com>
2020-10-26 22:15:58 -0400

    got essence working properly with fade effects and allows for orbit + particles as well.

--- Assets/Scripts/Ecosystems/Essenz.cs
+++ Assets/Scripts/Ecosystems/Essenz.cs
@@ -2,19 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Orbit))]
+/// <summary>
+/// Generic class for Essenz collectibles
+/// This is an Energy Currency, used between species.
+/// </summary>
 public class Essenz : AudioHandler
 {
     public AudioClip[] collectionSounds;
     public AudioClip[] donationSounds;
     PlayerController pc;
     Orbit orbitScript;
+    FadeSprite fadeSprite;
+    ParticleSystem particles;
 
     [Header("Essenz Settings")]
     public CollectibleStates cState;
     public enum CollectibleStates
     {
-        AWAIT, VORTEX, ORBIT, DONATE,
+        AWAIT, VORTEX, ORBIT, FADE, DONATE,
     }
     Vector3 donationFinal;
     public float vortexSpeed = 100f;
@@ -28,6 +33,10 @@ public class Essenz : AudioHandler
 
         pc = FindObjectOfType<PlayerController>();
         orbitScript = GetComponent<Orbit>();
+        fadeSprite = GetComponent<FadeSprite>();
+        if (fadeSprite == null)
+            fadeSprite = GetComponentInChildren<FadeSprite>();
+        particles = GetComponentInChildren<ParticleSystem>();
     }
 
     void Start ()
@@ -36,9 +45,24 @@ public class Essenz : AudioHandler
         vortexSpeed = Random.Range(vortexMin, vortexMax);
 
         //set orbit
-        orbitScript.orbiting = false;
-        orbitScript.orbitalSpeed = vortexSpeed;
-        orbitScript.planetToOrbit = pc.transform;
+        if (orbitScript)
+        {
+            orbitScript.orbiting = false;
+            orbitScript.orbitalSpeed = vortexSpeed;
+            orbitScript.planetToOrbit = pc.transform;
+        }
+
+        //keep that active
+        if (fadeSprite)
+        {
+            fadeSprite.keepActive = true;
+        }
+
+        //set particles
+        if (particles)
+        {
+            particles.Play();
+        }
 
         //await collection
         cState = CollectibleStates.AWAIT;
@@ -51,23 +75,23 @@ public class Essenz : AudioHandler
         {
             Vector3 pos = pc.transform.position;
 
-            transform.position = Vector3.Lerp(transform.position, pos, donationSpeed * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, pos, donationSpeed * Time.deltaTime);
 
             //when reaches mouth, send it into orbit
             if (Vector3.Distance(transform.position, pos) < minDist)
             {
-                SetOrbit(pos);
+                ConsumeCollectible();
             }
         }
 
         //donating to some source
         if (cState == CollectibleStates.DONATE)
         {
-            transform.position = Vector3.Lerp(transform.position, donationFinal, donationSpeed * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, donationFinal, donationSpeed * Time.deltaTime);
 
             if (Vector3.Distance(transform.position, donationFinal) < 1f)
             {
-                Destroy(gameObject);
+                ExpendCollectible();
             }
         }
     }
@@ -91,6 +115,30 @@ public class Essenz : AudioHandler
         Debug.Log("Vortexing to player");
     }
 
+    /// <summary>
+    /// Add to player inventory; set orbit, or fade;
+    /// </summary>
+    void ConsumeCollectible()
+    {
+        if (orbitScript)
+            SetOrbit(pc.transform.position);
+        if (fadeSprite)
+        {
+            //parent to player
+            transform.SetParent(pc.transform);
+            //fade
+            SetFade(true);
+        }
+        if (particles)
+            particles.Stop();
+
+        //add to player list
+        pc.essenceInventory.collectedEssenz.Add(this);
+
+        //consume sound
+        PlayRandomSoundRandomPitch(collectionSounds, 1f);
+    }
+
     //set orbit state
     void SetOrbit(Vector3 point)
     {
@@ -98,13 +146,38 @@ public class Essenz : AudioHandler
         orbitScript.orbiting = true;
     }
 
-    //called on donation decider
+    //fade sprite
+    void SetFade(bool fadeOut)
+    {
+        if (fadeOut)
+            fadeSprite.FadeOut();
+        else
+        {
+            fadeSprite.FadeIn();
+        }
+
+        cState = CollectibleStates.FADE;
+    }
+
+    /// <summary>
+    /// Can be called by other objects or characters to absorb some of the Player's energy.
+    /// </summary>
+    /// <param name="donationDest"></param>
     public void DonateEssenz(Transform donationDest)
     {
         orbitScript.orbiting = false;
 
         donationFinal = donationDest.position;
 
+        //fade in
+        if (cState == CollectibleStates.FADE)
+            SetFade(false);
+
+        //play particles again
+        if (particles)
+            particles.Play();
+
+        //set state
         cState = CollectibleStates.DONATE;
 
         //remove from player list
@@ -112,5 +185,20 @@ public class Essenz : AudioHandler
 
         //play a sound
         PlayRandomSoundRandomPitch(donationSounds, 1f);
+    }
+
+    /// <summary>
+    /// the final use case of the collected
+    /// </summary>
+    void ExpendCollectible()
+    {
+        if (fadeSprite)
+        {
+            fadeSprite.keepActive = false;
+            SetFade(true);
+            transform.SetParent(null);
+        }
+        else
+            Destroy(gameObject);
     }
 }
